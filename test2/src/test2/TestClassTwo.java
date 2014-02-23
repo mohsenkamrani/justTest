d6914add39a20895d0ff0acdc1a7c62d91919b11
mohsen <kamrani@ce.sharif.edu>
2014-02-23 09:40:53 +0330

    Adding a new Method.
    method:
    public void printMe() {}
    
    Signed-off-by: mohsen <kamrani@ce.sharif.edu>

--- test2/src/test2/TestClassTwo.java
+++ test2/src/test2/TestClassTwo.java
@@ -1,7 +1,12 @@
 package test2;
 
 public class TestClassTwo {
+	
 	TestClassTwo() {
 		System.out.println("Hi");
+	}
+	
+	public void printMe() {
+		System.out.println("This is the TestClassTwo");
 	}
 }
